2c05979c31cd325b0b58727a981e781f845bd1d4
Nc Galih <encegalih@gmail.com>
2024-01-18 14:48:21 +0700

    feat: create embeddings

--- controller/embedding.py
+++ controller/embedding.py
@@ -2,9 +2,12 @@ import os
 import json
 from dotenv import load_dotenv
 import requests
+import time
+import model
+import transform
+import threading
 
 load_dotenv()
-
 
 def get_embedding(text):
     url = "https://api.openai.com/v1/embeddings"
@@ -22,3 +25,43 @@ def get_embedding(text):
     response = requests.request("POST", url, headers=headers, data=payload)
 
     return response
+
+def embedding_content(content, source_title):
+    text = source_title + '\n' + content
+    response_embedding = get_embedding(text)
+    embedding_vector = response_embedding.data[0].embedding
+    token = response_embedding.usage.total_tokens
+    return [embedding_vector, token]
+
+def embedding_header(content, source_name, source_title):
+    header = transform.getHeader(source_name, source_title, content)
+    if header == None:
+        header_embedding_vector = None
+    else:
+        header_embedding = get_embedding(header)
+        header_embedding_vector = header_embedding.data[0].embedding
+    return header_embedding_vector
+
+def create_embeddings(source_id, header=False):
+    while True:
+        data = model.selectOne(source_id)
+        if not data:
+            print("completed")
+            break
+        [content, id, source_title, source_name] = data
+
+        [embedding_vector, token] = embedding_content(content, source_title)
+
+        header_embedding_vector = None
+        if header:
+            header_embedding_vector = embedding_header(content, source_name, source_title)
+
+        model.storeEmbedding(int(id), embedding_vector, int(token), header_embedding_vector)
+
+        print(id, token)
+        time.sleep(60/500)
+    return True
+
+def threaded_create_embeddings(source_id, header=False):
+    thread = threading.Thread(target=create_embeddings, args=(source_id, header))
+    thread.start()
