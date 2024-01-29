5886b25869a07f5f9a1c6b4445420348596b9dca
Nc Galih <encegalih@gmail.com>
2024-01-29 09:10:44 +0700

    tes doc = fitz.open(filepath)

--- controller/extract.py
+++ controller/extract.py
@@ -1,6 +1,8 @@
 import PyPDF2
+import fitz
 
 def extract_pdf(filepath):
+  """
   with open(filepath, 'rb') as file:
     pdf_reader = PyPDF2.PdfReader(file)
 
@@ -9,7 +11,13 @@ def extract_pdf(filepath):
     for i in range(len(pdf_reader.pages)):
       page = pdf_reader.pages[i]
       textpdf += page.extract_text()
+  """
+  doc = fitz.open(filepath)
 
+  for page_num in range(doc.page_count):
+    page = doc[page_num]
+    textpdf += page.get_text()
+    
   return textpdf
 
 def extract_pdf_per_page(filepath):
